61ec2cdafe2c5620146e2f7afb910cba185e14e8
ayeshaqadri <qadria392@gmail.com>
2024-12-04 14:40:06 +0500

    complete

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAuth,createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail  } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getFirestore ,collection, addDoc, deleteDoc, updateDoc, doc, getDocs, } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4RWWruUA2VquqYVLR23ipUEoPIzlKFJc",
+    authDomain: "project-dc717.firebaseapp.com",
+    projectId: "project-dc717",
+    storageBucket: "project-dc717.firebasestorage.app",
+    messagingSenderId: "404723403613",
+    appId: "1:404723403613:web:fc81e5a046cc163a2136b4",
+    measurementId: "G-LGWX5ZQ714"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth =getAuth(app);
+const db = getFirestore(app);
+export{
+    getAuth,app,auth,createUserWithEmailAndPassword, signInWithEmailAndPassword , sendPasswordResetEmail,db,getFirestore,collection, addDoc, deleteDoc, updateDoc, doc, getDocs 
+}
---
+++ login.js
@@ -0,0 +1,30 @@
+import { getAuth, signInWithEmailAndPassword } from "./firebase.js";
+
+let emailField = document.getElementById("useremail");
+let passwordField = document.getElementById("userpassword");
+let loginBtn = document.getElementById("singup-btn");
+
+loginBtn.addEventListener("click", () => {
+    if (emailField.value.trim() && passwordField.value.trim()) {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, emailField.value, passwordField.value)
+            .then((userCredential) => {
+                console.log("Login successful:", userCredential.user);
+                location.href = "index.html"; // Redirect to a dashboard page
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                Swal.fire({
+                    icon: "error",
+                    title: "Login Failed",
+                    text: errorMessage,
+                });
+            });
+    } else {
+        Swal.fire({
+            icon: "warning",
+            title: "Incomplete Form",
+            text: "Please fill out all fields.",
+        });
+    }
+});
